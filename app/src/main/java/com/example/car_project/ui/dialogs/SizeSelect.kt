d639eb2832c4c2cf0f3c99945bdc10cd7c9c821e
rongo <rongo123321@gmail.com>
2025-04-23 23:35:42 +0300

    game start size start (made it a //)

--- app/src/main/java/com/example/car_project/ui/dialogs/SizeSelect.kt
+++ app/src/main/java/com/example/car_project/ui/dialogs/SizeSelect.kt
@@ -1,29 +1,29 @@
 package com.example.car_project.ui.dialogs
 
 object SizeSelect {
-    private fun showSizeSelectionDialog() {
-        val dialogView = layoutInflater.inflate(R.layout.dialog_select_size, null)
-        val dialog = Dialog(this)
-        dialog.setContentView(dialogView)
-        dialog.setCancelable(false)
-        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
-
-        dialogView.findViewById<Button>(R.id.btn_small).setOnClickListener {
-            dialog.dismiss()
-            startGameWithSize(Small)
-        }
-
-        dialogView.findViewById<Button>(R.id.btn_medium).setOnClickListener {
-            dialog.dismiss()
-            startGameWithSize(Medium)
-        }
-
-        dialogView.findViewById<Button>(R.id.btn_large).setOnClickListener {
-            dialog.dismiss()
-            startGameWithSize(Large)
-        }
-
-        dialog.show()
-    }
+//    private fun showSizeSelectionDialog() {
+//        val dialogView = layoutInflater.inflate(R.layout.dialog_select_size, null)
+//        val dialog = Dialog(this)
+//        dialog.setContentView(dialogView)
+//        dialog.setCancelable(false)
+//        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+//
+//        dialogView.findViewById<Button>(R.id.btn_small).setOnClickListener {
+//            dialog.dismiss()
+//            startGameWithSize(Small)
+//        }
+//
+//        dialogView.findViewById<Button>(R.id.btn_medium).setOnClickListener {
+//            dialog.dismiss()
+//            startGameWithSize(Medium)
+//        }
+//
+//        dialogView.findViewById<Button>(R.id.btn_large).setOnClickListener {
+//            dialog.dismiss()
+//            startGameWithSize(Large)
+//        }
+//
+//        dialog.show()
+//    }
 
 }
